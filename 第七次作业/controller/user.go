3731cb210bdc37550401f42c0c93986781c0aa4f
Yuqin425 <114483458+Yuqin425@users.noreply.github.com>
2022-12-03 14:14:07 +0800

    第七次作业

---
+++ 第七次作业/controller/user.go
@@ -0,0 +1,113 @@
+package controller
+
+import (
+	"goPractice/Week_08/dao"
+	"goPractice/Week_08/model"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var db = dao.InitDB()
+
+func Register(ctx *gin.Context) {
+
+	name := ctx.PostForm("name")
+	telephone := ctx.PostForm("telephone")
+	password := ctx.PostForm("password")
+
+	//数据验证
+	if len(name) == 0 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code":    422,
+			"message": "用户名不能为空",
+		})
+		return
+	}
+	if len(telephone) != 11 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code":    422,
+			"message": "手机号必须为11位",
+		})
+		return
+	}
+	if len(password) < 6 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code":    422,
+			"message": "密码不能少于6位",
+		})
+		return
+	}
+
+	//判断手机号是否存在
+	var user model.User
+	db.Where("telephone = ?", telephone).First(&user)
+	if user.ID != 0 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code":    422,
+			"message": "用户已存在",
+		})
+		return
+	}
+
+	//创建用户
+	newUser := model.User{
+		Name:      name,
+		Telephone: telephone,
+		Password:  password,
+	}
+	db.Create(&newUser)
+
+	//返回结果
+	ctx.JSON(http.StatusOK, gin.H{
+		"code":    200,
+		"message": "注册成功",
+	})
+}
+
+func Login(ctx *gin.Context) {
+
+	telephone := ctx.PostForm("telephone")
+	password := ctx.PostForm("password")
+
+	//数据验证
+	if len(telephone) != 11 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code":    422,
+			"message": "手机号必须为11位",
+		})
+		return
+	}
+	if len(password) < 6 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code":    422,
+			"message": "密码不能少于6位",
+		})
+		return
+	}
+
+	//判断手机号是否存在
+	var user model.User
+	db.Where("telephone = ?", telephone).First(&user)
+	if user.ID == 0 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code":    422,
+			"message": "用户不存在",
+		})
+		return
+	}
+
+	//判断密码是否正确
+	if password != user.Password {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code":    422,
+			"message": "密码错误",
+		})
+	}
+
+	//返回结果
+	ctx.JSON(http.StatusOK, gin.H{
+		"code":    200,
+		"message": "登录成功",
+	})
+}
